edc36d70a1f983cd3f97657b77f90ae7475c588b
Dan Dascalescu <ddascalescu+github@gmail.com>
2014-12-16 16:43:53 -0800

    Initial commit

---
+++ client/parent-data.js
@@ -0,0 +1,22 @@
+var players = [{name: 'foo', age: 30}, {name: 'bar', age: 20}];
+
+Template.body.helpers({
+  players: players,
+  team: {
+    captain: {
+      name: 'baz - only here does parentData(1) work correctly'
+    }
+  }
+});
+
+['player', 'playerWrapper', 'myEach', 'body'].forEach(function (where) {
+  Template[where].events({
+    'click': function () {
+      console.group('--- from %s ---', where);
+      console.log('Current data', Template.currentData());
+      console.log('Parent(0) data', Template.parentData(0));
+      console.log('Parent(1) data', Template.parentData(1));
+      console.groupEnd();
+    }
+  })
+});
